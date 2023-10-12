1157eb1c40ad80e72d8f96240f0dd5f9cecbc3ec
Edocico <edoardo.cico@gmail.com>
2023-10-12 18:23:52 +0200

    add funzione di confronto array

--- js/app.js
+++ js/app.js
@@ -1,40 +1,47 @@
-console.log('ok')
+//console.log("ok");
 
-const paliBtnDOMElement = document.getElementById('pali-input-btn')
-console.dir(paliBtnDOMElement)
-const paliTextDOMElement = document.querySelector('.pali-input-text')
-console.dir(paliTextDOMElement)
+const paliBtnDOMElement = document.getElementById("pali-input-btn");
+console.dir(paliBtnDOMElement);
+const paliTextDOMElement = document.querySelector(".pali-input-text");
+console.dir(paliTextDOMElement);
 
- 
-
-paliBtnDOMElement.addEventListener('click', function() {
-    console.log(paliTextDOMElement.value)
-    if (isPalindrom(paliTextDOMElement.value) === 1) {
-        alert('la parola è palindroma')
-    } else {
-        alert('la parola non è palindroma')
-    }
-    
-    
-})  
+paliBtnDOMElement.addEventListener("click", function () {
+  console.log(paliTextDOMElement.value);
+  if (isPalindrom(paliTextDOMElement.value)) {
+    alert("la parola è palindroma");
+  } else {
+    alert("la parola non è palindroma");
+  }
+});
 
 // funzione verifica parola palindroma
 function isPalindrom(inputWord) {
-    //let array = []
-    let reverseArray = []
-    const wordArray = Array.from(inputWord)
-    console.log(wordArray)
-    /* for (let i = 0; i < wordArray.length; i++) {
-        array.push(wordArray[i])
+  
+  let reverseArray = [];
+  const wordArray = Array.from(inputWord);
+  console.log(wordArray);
+  
+  for (let i = wordArray.length - 1; i >= 0; i--) {
+    reverseArray.push(wordArray[i]);
+  }
+  console.log(reverseArray);
+  if (arrayEquals(wordArray, reverseArray)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+// funzione di confronto array // Samuel //
+function arrayEquals(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    let uguali = true;
+    for (let index = 0; index < arr1.length; index++) {
+      if (arr1[index] !== arr2[index]) {
+        uguali = false;
+      }
     }
-    console.log(array) */
-    for (let i = wordArray.length - 1; i >= 0; i--) {
-        reverseArray.push(wordArray[i])
-    }
-    console.log(reverseArray)
-    if (wordArray === reverseArray) {
-        return true
-    } else {
-        return false
-    }
+    return uguali;
+  }
 }
